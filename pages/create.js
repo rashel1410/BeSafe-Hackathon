e0eb5a42eecfe673a88a3a7772fbcf1c13b4eee3
Sedri14 <sedri14@gmail.com>
2024-01-28 22:48:02 +0200

    delete some imports

--- pages/create.js
+++ pages/create.js
@@ -1,5 +1,3 @@
-import ToggleButton from '@mui/material/ToggleButton';
-import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import FacebookRoundedIcon from '@mui/icons-material/FacebookRounded';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import Radio from '@mui/material/Radio';
