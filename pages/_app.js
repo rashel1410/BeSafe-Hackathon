23a3fd39a0986df6925045767cc138e47f2a87e9
Sedri14 <sedri14@gmail.com>
2024-02-03 13:54:07 +0200

    rtl

--- pages/_app.js
+++ pages/_app.js
@@ -5,9 +5,11 @@ import { AppProvider } from '../public/context';
 function App({ Component, pageProps }) {
   return (
     <AppProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <div dir='rtl'>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </div>
     </AppProvider>
   );
 }
