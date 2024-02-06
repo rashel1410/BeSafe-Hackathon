81524f336928a69ec7548421c98811483cc71b4c
MayaIw <mayaiwanir@mail.tau.ac.il>
2024-02-06 15:19:48 +0200

    finished game

--- helpers/Questions.js
+++ helpers/Questions.js
@@ -1,11 +1,20 @@
 export const Questions = [
   {
+    title: "שם משתמש #1",
     optionA: "fakeTaylor.jpg",
     optionB: "trueTaylor.jpg",
     answer: "A",
     explanation: "בפרופיל המזויף, לא מופיע סימן וי, שמסמן שהחשבון מאומת",
   },
   {
+    title: "שם משתמש #2",
+    optionA: "correct_spelling.jpg",
+    optionB: "misspelled.jpg",
+    answer: "B",
+    explanation: "בפרופיל המזויף, שם המשתמש כתוב עם שגיאות כתיב.",
+  },
+  {
+    title: "תמונת פרופיל",
     optionA: "generic.jpg",
     optionB: "authentic.jpg",
     answer: "A",
@@ -13,12 +22,7 @@ export const Questions = [
       "בפרופיל המזויף, תמונת הפרופיל גנרית- ניתן למצוא אותה בקלות באינטרנט. ",
   },
   {
-    optionA: "correct_spelling.jpg",
-    optionB: "misspelled.jpg",
-    answer: "B",
-    explanation: "בפרופיל המזויף, שם המשתמש כתוב עם שגיאות כתיב.",
-  },
-  {
+    title: "חברים משותפים",
     optionA: "low_mutual_friends.jpg",
     optionB: "high_mutual_friends.jpg",
     answer: "A",
@@ -26,10 +30,19 @@ export const Questions = [
       "בפרופיל המזויף, כמות החברים המשותפים איתך נמוך מאוד. כנראה זה לא אדם שאת מכירה.",
   },
   {
+    title: "עוקבים",
     optionA: "high_followers.jpg",
     optionB: "low_followers.jpg",
     answer: "B",
     explanation:
       "בפרופיל המזויף, כמות האנשים אחריהם הוא עוקב גבוהה משמעותית מכמות העוקבים שלו. בפרופיל הלגימטימי, היחס יותר מאוזן.",
+  },
+  {
+    title: "תאריך יצירה",
+    optionA: "old.jpg",
+    optionB: "recent.jpg",
+    answer: "B",
+    explanation:
+      "הפרופיל המזויף נוצר לאחרונה. הפרופיל הלגימטימי נוצר לפני מספר שנים, וניתן לבדוק את אמינותו בעזרת היסטוריית הפרסומים",
   },
 ];
