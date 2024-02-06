0dced4481ebfb73f7061f310dcb478e2276d1cf7
Rashel <rashstar14@gmail.com>
2024-02-06 16:53:35 +0200

    card changes

--- comps/Card.js
+++ comps/Card.js
@@ -4,17 +4,18 @@ import styles from '../styles/Card.module.css';
 import Search from './Search';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
 import AvatarList from './AvatarList'
-import { red } from '@mui/material/colors';
-import Fab from '@mui/material/Fab';
-import AddIcon from '@mui/icons-material/Add';
-
 
 const Card = ({ title, content, searchBar = false, avatarList = false, addButton = false, clickHandler, url}) => {
+    let app;
+    if(title === 'אינסטגרם')
+    {
+        app = 'Instagram';
+    }
+    else{
+        app = 'Facebook'
+    }
+
     const cardContent = () => (
         <div className={styles.card}>
             <div className={styles.card_content}>
@@ -28,7 +29,7 @@ const Card = ({ title, content, searchBar = false, avatarList = false, addButton
                         </Typography>
                     )}
 
-                    {searchBar && <Search />}
+                    {searchBar && <Search app={app}/>}
 
                     
                     {avatarList && (
