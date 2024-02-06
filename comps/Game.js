08783444925bf8a64513e9abbf06919bec84f4d7
tanya1995-31 <tanyagendelman@gmail.com>
2024-02-06 17:15:54 +0200

    game style

--- comps/Game.js
+++ comps/Game.js
@@ -3,7 +3,8 @@ import { useState, useContext } from "react";
 import { Questions } from "../helpers/Questions";
 import { GameContext } from "../helpers/Contexts";
 import { Box } from "@mui/material";
-
+import styles from '/styles/Card.module.css';
+import Card from '/comps/Card.js';
 const Game = () => {
   const [currQuestion, setCurrQuestion] = useState(0);
   const [optionChosen, setOptionChosen] = useState("");
@@ -12,12 +13,13 @@ const Game = () => {
     <div className="Game">
       <h2>{Questions[currQuestion].title}</h2>
       <div className="options">
+        <Card content = {(<Box sx={{display:"flex" ,flexDirection:"row",gap:"10px"}}>
         <button onClick={() => setOptionChosen("A")}>
           <Box
             component="img"
             sx={{
-              maxHeight: { xs: 300 },
-              maxWidth: { xs: 500 },
+              maxHeight: { xs: 200 },
+              maxWidth: { xs: 400 },
             }}
             src={Questions[currQuestion].optionA}
           />
@@ -26,12 +28,15 @@ const Game = () => {
           <Box
             component="img"
             sx={{
-              maxHeight: { xs: 300 },
-              maxWidth: { xs: 500 },
+              maxHeight: { xs: 200 },
+              maxWidth: { xs: 400 },
             }}
             src={Questions[currQuestion].optionB}
           />
         </button>
+        </Box>
+        )}></Card>
+       
         {Questions[currQuestion].answer == optionChosen && <h2>תשובה נכונה</h2>}
         {optionChosen != "" &&
           Questions[currQuestion].answer != optionChosen && (
